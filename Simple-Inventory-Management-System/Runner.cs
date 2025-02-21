b36e3328e99f6ea530871456f2e776480648990c
AbdullahSholi <groupgroup060@gmail.com>
2025-02-21 16:33:18 +0200

    Initial commit

---
+++ Simple-Inventory-Management-System/Runner.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Simple_Inventory_Management_System
+{
+    public class Runner
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
